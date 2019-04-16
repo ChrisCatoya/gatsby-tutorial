4d0988cd446506032240f9de0e07017f20a22dc4
Chris Catoya <christopher.catoya@gmail.com>
2019-04-16 13:55:04 -0700

    first commit

---
+++ gatsby-node.js
@@ -0,0 +1,33 @@
+// uses require syntax not import
+const path = require('path');
+
+exports.createPages = ({graphql, actions}) => {
+    const {createPage} = actions;  // same as actions.createPage
+    return new Promise((resolve, reject) => {
+        graphql (`
+        {
+            allMarkdownRemark { 
+                edges {
+                 node {
+                  frontmatter{
+                    title
+                    slug
+                  }
+                 } 
+                }
+              }
+            }
+          `).then(results => {
+            results.data.allMarkdownRemark.edges.forEach(({node}) => {
+                createPage({
+                    path: `/posts${node.frontmatter.slug}`,
+                    component: path.resolve('./src/components/postLayout.js'),
+                    context: {
+                        slug: node.frontmatter.slug,
+                    }
+                });
+            })
+            resolve();
+          })
+    });
+}
